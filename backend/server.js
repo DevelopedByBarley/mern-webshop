76158adaa7de30f160343faa824df41179959851
Árpád <developedbybarley@gmail.com>
2022-12-04 13:42:45 +0100

    heroku test

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,14 @@ const orderRouter = require('./routes/orderRouter')
 
 const connectDb = require('./database/db/connectDb')
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
 connectDb();
 
 app.use(express.static(__dirname + '/public'))
