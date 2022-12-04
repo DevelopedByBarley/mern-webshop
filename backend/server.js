def60eacf230cca810380793898225553a46721d
Árpád <developedbybarley@gmail.com>
2022-12-04 14:21:44 +0100

    heroku test

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,15 @@ const orderRouter = require('./routes/orderRouter')
 const connectDb = require('./database/db/connectDb')
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
 }
+
+export default app;
 
 
 connectDb();
