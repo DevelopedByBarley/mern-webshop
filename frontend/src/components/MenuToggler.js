abdc9aa7d70c40624c66554dd730b8b578b56d7b
Szaniszló Árpád <developedbybarley@gmail.com>
2022-12-25 14:48:38 +0100

    menu toggler remake

--- frontend/src/components/MenuToggler.js
+++ frontend/src/components/MenuToggler.js
@@ -1,8 +1,11 @@
 import '../styles/components/MenuToggler.css'
-import { GiHamburgerMenu } from 'react-icons/gi'
 
 export function MenuToggler({ setMenuToggle, isMenuToggle }) {
+
   return (
-    <div className='menu-toggle' onClick={() => { setMenuToggle(!isMenuToggle); console.log(isMenuToggle) }}><GiHamburgerMenu color='white' size={30}/></div>
+    <div className='menu-toggle' onClick={() => { setMenuToggle(!isMenuToggle) }}>
+      <div className={`top-line ${isMenuToggle ? "active" : ""}`}></div>
+      <div className={`bottom-line ${isMenuToggle ? "active" : ""}`}></div>
+    </div>
   )
 }
